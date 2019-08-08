78ac99f55352806caa5168a3b5924771f95a493e
snow <mz65918@gmail.com>
2019-08-08 22:19:36 +0900

    ABC136_D AC

--- ABC136_D.py
+++ ABC136_D.py
@@ -1,11 +1,29 @@
 S = input()
 
-countR = 0
-answer = []
-for i in range(len(S)):
+count = 1
+answer = [0] * len(S)
+# RLの切り替わるインデックスをマーク
+n = 0
+for i in range(len(S) - 1):
     if (S[i] == S[i + 1]):
-        if (S[i] == 'R'):
-            countR += 1
-            answer.append[0]
+        count += 1
     else:
-        
+        if (S[i + 1] == 'R'):
+            # Lを割り振る
+            answer[n] += count // 2
+            answer[n + 1] += -(-count // 2)
+            count = 1
+        else:
+            # Rを割り振る
+            answer[i] += -(-count // 2)
+            answer[i + 1] += count // 2
+            count = 1
+            n = i
+    # 右端のLを割り振る
+    if (i == len(S) - 2):
+        answer[n] += count // 2
+        answer[n + 1] += -(-count // 2)
+
+for i, c in enumerate(answer):
+    print(c, end='')
+    print(' ', end='')
