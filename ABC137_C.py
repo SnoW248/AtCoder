d4e829dc95a96a29fea8a000d66b18494e1a9d88
snow <mz65918@gmail.com>
2019-08-18 18:13:59 +0900

    ABC137 AC

--- ABC137_C.py
+++ ABC137_C.py
@@ -1,23 +1,15 @@
 N = int(input())
 
-s = []
-dictList = []
-for i in range(N):
-    d = {}
-    # 受け取った文字列を辞書型に{文字: 数}
-    s.append(input())
-    for c in s[i]:
-        if c in d:
-            d[c] += 1
-        else:
-            d[c] = 1
-    # 比較がしやすいようにソートしたものを追加
-    dictList.append(sorted(d.items()))
-
+s = ''.join(sorted(input()))
+h = {s: 1}
 count = 0
-for i in range(len(dictList)):
-    for j in range(i + 1, len(dictList)):
-        if dictList[i] == dictList[j]:
-            count += 1
+for i in range(N - 1):
+    # 比較がしやすいようにソートしたものを追加
+    s = ''.join(sorted(input()))
+    if s in h:
+        count += h[s]
+        h[s] += 1
+    else:
+        h[s] = 1
 
 print(count)
